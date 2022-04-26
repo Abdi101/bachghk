982cd53df24966627af85ec23fc85d6db2523857
unknown <abdi58094@gmail.com>
2022-04-26 17:22:10 +0200

    update the teams component

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,9 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: [
-      'myKeyA',
-      'myKeyB'
-    ]
+    keys: env.array('APP_KEYS', ['testkey1','testKey2']),
   },
 });
